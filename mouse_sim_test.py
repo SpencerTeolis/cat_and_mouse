400dc70e1293b0e289b880e6565e2a7569df4b10
SpencerTeolis <spe1.teolis@gmail.com>
2020-06-17 05:19:52 -0400

    Cleanup, add comments

--- mouse_sim_test.py
+++ mouse_sim_test.py
@@ -5,50 +5,38 @@ from scene import Boundary
 from mouse import Mouse, repel_vector, towards_cat
 from util_funcs import *
 
-
-points = np.load("calibration/calibration_points.npy")
-# p = np.transpose(points)
-# mask = p[0]
-# mask[mask<100] = 0
-# mask[mask>1000] = 0
-# points = np.transpose(p[:,mask.astype(bool)])
+disp_shape = (720,1280,3)
 point = np.asarray([300,400])
 
-kwargs = {'calibration_points' : points, 'middle_point' : point}
-boundary = Boundary(**kwargs)
-
-dispW=1280 
-dispH=720
-disp_shape = (dispH,dispW,3)
-
-img = np.zeros(disp_shape)
+# Set Boundary
+boundary = Boundary(calibration_points = np.load("calibration/calibration_points.npy"), middle_point = point)
+bounds = np.zeros(disp_shape)
 for x,y in boundary.lines:
-    cv2.line(img,tuple(x),tuple(y),(0,0,255),2)
+    cv2.line(bounds,tuple(x),tuple(y),(0,0,255),2)
 
-mouse_img = np.zeros_like(img)
+# Create mouse
+mouse_img = np.zeros_like(bounds)
 mouse = Mouse(point, boundary, repel_vector, towards_cat)
 mouse.draw_mouse(mouse_img)
 
+# Move 'cat' with cursor
 def cat(event, x, y, flags, param):
     img, cat_pos = param
     if event == 0:
-        #print(x,y)
-        img[img[:,:,1].astype(bool)] = [0, 0, 0]
+        cv2.circle(img, tuple(cat_pos), 6, (0,0,0),-1)
         cv2.circle(img, (x,y), 6, (0,255,0),-1)
-        cat_pos[0] = x
-        cat_pos[1] = y
+        cat_pos[:] = x, y
 
 cat_pos = point + 100
 cv2.namedWindow("image")
-cv2.setMouseCallback("image", cat, (img, cat_pos))
+cv2.setMouseCallback("image", cat, (bounds, cat_pos))
 
-curr_time = time.time()
+# Main loop
 while True:
-    #if time.time() - curr_time > .015:
     mouse.update_position(cat_pos)
-    mouse_img = mouse.draw_mouse((dispH,dispW,3))
+    mouse_img = mouse.draw_mouse(disp_shape)
     cv2.imshow("image", img+mouse_img)
-    curr_time = time.time()
+
     key = cv2.waitKey(300) & 0xFF 
 
     if key == ord("q"):
