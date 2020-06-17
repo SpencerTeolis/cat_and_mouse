af465ea20099a58f6efe3b26253b8fbb9cb9a8fe
Spencer Teolis <spencerteolis@Spencers-MBP.fios-router.home>
2020-06-17 04:07:49 -0400

    Refactor to use tracking classes

--- cat_and_mouse.py
+++ cat_and_mouse.py
@@ -4,42 +4,41 @@ import time
 from adafruit_servokit import ServoKit
 from scene import Boundary
 from mouse import Mouse, repel_vector, towards_cat
+from tracking import TrackObject, Threshold
 from util_funcs import *
 
-def nothing(x):
-    pass
-
-cv2.namedWindow('TrackBars')
-cv2.moveWindow('TrackBars', 0,0)
-
-trackbar_names = ['RedLow','RedHigh','GreenLow','GreenHigh','BlueLow','BlueHigh']
-init_vals = [0,70,0,100,0,64]
-max_val = 255
-
-for name, val in zip(trackbar_names, init_vals):
-    cv2.createTrackbar(name, 'TrackBars',val,max_val,nothing)
-
 kit = ServoKit(channels=16)
-#3264x2464
 dispW=1280 
 dispH=720
 flip=0
 
+# set up camera and image window
 camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=30/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
 cam=cv2.VideoCapture(camSet)
+cv2.namedWindow("image")
+cv2.moveWindow("image", 200,0)
 
-# Define threshold values for tracking object
-ret, frame = cam.read()
-print(frame.shape)
+# Load servo coordinates to image coordinates maps
+points = np.load("calibration/calibration_points.npy")
+interp_p = np.nan_to_num(np.load("calibration/interp_p.npy"))
+interp_t = np.nan_to_num(np.load("calibration/interp_t.npy"))
+
+# Set bounds
+kwargs = {'calibration_points' : points, 'middle_point' : np.asarray([dispW//2,dispH//2],np.uint16)}
+boundary = Boundary(**kwargs)
+bounds = np.zeros((dispH,dispW))
+for x,y in boundary.lines:
+    cv2.line(bounds,tuple(x),tuple(y),255,2)
+
+# Create tracking object
+thresh = TrackObject(Threshold,init_vals = [0,0,0,70,100,64])
+
+# Get values for thresholding
 while True:
     ret, frame = cam.read()
 
-    l_b = np.asarray([cv2.getTrackbarPos(name,'TrackBars') for name in trackbar_names[::2]])
-    u_b = np.asarray([cv2.getTrackbarPos(name,'TrackBars') for name in trackbar_names[1::2]])
-
-    laserMask = cv2.inRange(frame,l_b,u_b)
-    cv2.imshow('laserMask', laserMask)
-    cv2.moveWindow('laserMask',610,610)
+    img = thresh.get_segmentation(frame)
+    cv2.imshow('image', img)
 
     if cv2.waitKey(1)==ord('q'):
         break
@@ -47,33 +46,12 @@ while True:
 cv2.destroyAllWindows()
 
 # Get other objects in the scene that have the same threshold values as the tracking object
-blackMask = np.zeros((dispH,dispW))
+frames = []
 for i in range(50):
-    ret, frame = cam.read()
+    frames.append(cam.read()[1])
     time.sleep(0.01)
-    blackMask = np.logical_or(blackMask, cv2.inRange(frame,l_b,u_b+10))
 
-blackMask = blackMask.astype(np.uint8)*255
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
-blackMask = cv2.dilate(blackMask,kernel,iterations=1) 
-
-
-points = np.load("calibration/calibration_points.npy")
-p = np.transpose(points)
-mask = p[0]
-mask[mask<100] = 0
-mask[mask>1000] = 0
-points = np.transpose(p[:,mask.astype(bool)])
-
-interp_p = np.nan_to_num(np.load("calibration/interp_p.npy"))
-interp_t = np.nan_to_num(np.load("calibration/interp_t.npy"))
-
-kwargs = {'calibration_points' : points, 'middle_point' : point}
-boundary = Boundary(**kwargs)
-
-bounds = np.zeros((dispH,dispW))
-for x,y in boundary.lines:
-    cv2.line(bounds,tuple(x),tuple(y),255,2)
+thresh.set_background_mask(frames)
 
 mouse = Mouse(point, boundary, repel_vector, towards_cat)
 dispBounds = np.asarray([dispW,dispH])
@@ -88,13 +66,6 @@ def get_cat_pos(frame):
         cX = int(M['m10']/M['m00']) 
         cY = int(M['m01']/M['m00']) 
 
-    # img = np.zeros((dispH,dispW,3))
-    # img[momoMask.astype(bool),0] = 255
-    # img[bounds.astype(bool),1] = 255
-    # img[mouse.astype(bool),2] = 255
-
-    # cv2.circle(img,(cX, cY),6,(255,255,0),-1)
-    # cv2.imshow('momoMask', img)
     return cX, cY
 
 def move_mouse(mouse, cat_pos):
