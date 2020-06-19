d471fb56ab631fecf0703522f0031810597818b0
Spencer Teolis <spencerteolis@Spencers-MBP.fios-router.home>
2020-06-19 19:21:54 -0400

    Refactor for changes in scene.py

--- mouse_sim_test.py
+++ mouse_sim_test.py
@@ -3,8 +3,7 @@ import cv2
 import time
 import display
 from scene import Boundary
-from mouse import Mouse, repel_vector, towards_cat
-from util_funcs import *
+from mouse import Mouse, repel_all, towards_cat
 
 dispH, dispW = 720, 1280
 img = np.zeros((dispH, dispW, 3))
@@ -14,14 +13,14 @@ mouse_start = np.asarray([dispW//2, dispH//2])
 # Set Boundary
 boundary = Boundary(calibration_points = np.load("calibration/calibration_points.npy"), 
                     middle_point = mouse_start)
-for p1, p2 in boundary.lines:
-    cv2.line(img, tuple(p1), tuple(p2), (0,0,255), 2)
+pts = boundary.vertices.reshape(-1,1,2)
+cv2.polylines(img,[pts],True,(0,0,255))
 
 cat_pos = boundary.get_point_in_bounds().astype(np.int16)
 cv2.circle(img, tuple(cat_pos), 6, (0,255,0),-1)
 
 # Create mouse
-mouse = Mouse(mouse_start, boundary, repel_vector, towards_cat)
+mouse = Mouse(mouse_start, boundary, repel_all, towards_cat)
 
 # Move 'cat' with cursor
 def cat(event, x, y, flags, param):
