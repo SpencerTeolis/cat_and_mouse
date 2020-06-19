547a49116ade0dd28d0e420de849ecd600b5603e
Spencer Teolis <spencerteolis@Spencers-MBP.fios-router.home>
2020-06-18 23:56:41 -0400

    Clean up + start cat at random position in bounds

--- mouse_sim_test.py
+++ mouse_sim_test.py
@@ -6,18 +6,22 @@ from scene import Boundary
 from mouse import Mouse, repel_vector, towards_cat
 from util_funcs import *
 
-disp_shape = (720,1280,3)
-point = np.asarray([300,400])
+dispH, dispW = 720, 1280
+img = np.zeros((dispH, dispW, 3))
+
+mouse_start = np.asarray([dispW//2, dispH//2])
 
 # Set Boundary
-boundary = Boundary(calibration_points = np.load("calibration/calibration_points.npy"), middle_point = point)
-bounds = np.zeros(disp_shape)
-for x,y in boundary.lines:
-    cv2.line(bounds,tuple(x),tuple(y),(0,0,255),2)
+boundary = Boundary(calibration_points = np.load("calibration/calibration_points.npy"), 
+                    middle_point = mouse_start)
+for p1, p2 in boundary.lines:
+    cv2.line(img, tuple(p1), tuple(p2), (0,0,255), 2)
+
+cat_pos = boundary.get_point_in_bounds().astype(np.int16)
+cv2.circle(img, tuple(cat_pos), 6, (0,255,0),-1)
 
 # Create mouse
-mouse = Mouse(point, boundary, repel_vector, towards_cat)
-img = np.copy(bounds)
+mouse = Mouse(mouse_start, boundary, repel_vector, towards_cat)
 
 # Move 'cat' with cursor
 def cat(event, x, y, flags, param):
@@ -32,8 +36,6 @@ def update(img, mouse=None):
     mouse.update_position(cat_pos)
     cv2.circle(img, tuple(mouse.position), 6, (0,0,255),-1)
 
-cat_pos = point + 100
 cv2.namedWindow("image")
 cv2.setMouseCallback("image", cat, (img, cat_pos))
-
 display.display_img(img, "image", update, mouse=mouse)
