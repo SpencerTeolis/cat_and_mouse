f0f0715a77483b64cd7bce6397c66d021d1650b7
SpencerTeolis <spe1.teolis@gmail.com>
2020-06-19 20:34:19 -0400

    Refactor, still need fix camera issues

--- cat_and_mouse.py
+++ cat_and_mouse.py
@@ -1,22 +1,29 @@
 import numpy as np
 import cv2
 import time
+import display
 from adafruit_servokit import ServoKit
 from scene import Boundary
 from mouse import Mouse, repel_all, towards_cat
 from tracking import TrackObject, Threshold
 from util_funcs import *
 
-kit = ServoKit(channels=16)
-dispW=1280 
-dispH=720
-flip=0
+#TODO: camera lagging
 
-# set up camera and image window
-camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=30/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
-cam=cv2.VideoCapture(camSet)
+# Set up servos
+kit = ServoKit(channels=16)
+
+# Set up camera and image window
+dispW, dispH = 1280, 720
+cam = display.set_camera(dispW, dispH)
 cv2.namedWindow("image")
 cv2.moveWindow("image", 200,0)
+
+# Create tracking object for laser
+thresh = TrackObject(Threshold,init_vals = [0,0,0,70,100,64])
+
+# Get values for thresholding
+display.display_cam(cam, "image", thresh.get_segmentation)
 
 # Load servo coordinates to image coordinates maps
 points = np.load("calibration/calibration_points.npy")
@@ -24,68 +31,55 @@ interp_p = np.nan_to_num(np.load("calibration/interp_p.npy"))
 interp_t = np.nan_to_num(np.load("calibration/interp_t.npy"))
 
 # Set bounds
-point = np.asarray([dispW//2,dispH//2],np.uint16)
+point = np.asarray([dispW//2,dispH//2],np.int16)
+p = np.transpose(points)
+mask = p[0]
+mask[mask<100] = 0
+mask[mask>1000] = 0
+points = np.transpose(p[:,mask.astype(bool)])
 kwargs = {'calibration_points' : points, 'middle_point' : point}
 boundary = Boundary(**kwargs)
 bounds = np.zeros((dispH,dispW))
-for x,y in boundary.lines:
-    cv2.line(bounds,tuple(x),tuple(y),255,2)
-
-# Create tracking object
-thresh = TrackObject(Threshold,init_vals = [0,0,0,70,100,64])
+pts = boundary.vertices.reshape(-1,1,2)
+cv2.polylines(bounds,[pts],True,(0,0,255))
 
 # Get values for thresholding
-while True:
-    ret, frame = cam.read()
-
-    img = thresh.get_segmentation(frame)
-    cv2.imshow('image', img)
-
-    if cv2.waitKey(1)==ord('q'):
-        break
-
-cv2.destroyAllWindows()
+display.display_cam(cam, "image", thresh.get_segmentation)
 
 # Get other objects in the scene that have the same threshold values as the tracking object
 frames = []
-for i in range(50):
+for i in range(20):
     frames.append(cam.read()[1])
     time.sleep(0.01)
 
-thresh.seg.set_background_mask(frames)
+thresh.seg.set_background_mask(frames, 2)
+cv2.imshow('image', thresh.seg.bg_mask.astype(np.uint8)*255)
+
+if cv2.waitKey(0)==ord('q'):
+    sys.exit(0)
 
 mouse = Mouse(point, boundary, repel_all, towards_cat)
 dispBounds = np.asarray([dispW,dispH])
 
-def get_cat_pos(frame):
-    img = thresh.get_segmentation(frame) 
+def update(frame, mouse, time_l):
+    img = thresh.get_segmentation(frame)
+    cat_pos = thresh.get_midpoint(img.any(axis=-1).astype(np.uint8))
 
-    return thresh.get_midpoint(img.any(axis=-1).astype(np.uint8))
+    if time.time() - time_l[0] > 0.015:
+        mouse.update_position(cat_pos)
+        x, y = mouse.position
+        pan = interp_p[x, y]
+        tilt = interp_t[x, y]
+        if pan and tilt:
+            kit.servo[0].angle=pan
+            kit.servo[1].angle=tilt
+        
+        time_l[0] = time.time()
 
-def move_mouse(mouse, cat_pos):
-    mouse.update_position(cat_pos)
+    return img
 
-    x, y = np.minimum(np.maximum(mouse.position,0),dispBounds).astype(np.uint16)
-    pan = interp_p[x, y]
-    tilt = interp_t[x, y]
-    if pan and tilt:
-        kit.servo[0].angle=pan
-        kit.servo[1].angle=tilt
+time_l = [time.time()]
+display.display_cam(cam, "image", update, mouse=mouse, time_l=time_l)
 
-
-curr_time = time.time()
-while True:
-    ret, frame = cam.read()
-
-    if time.time() - curr_time > 0.015:
-        cat_pos = np.asarray(get_cat_pos(frame))
-        move_mouse(mouse, cat_pos)
-        curr_time = time.time()
-
-    #cv2.imshow("image", img+mouse)
-    key = cv2.waitKey(1) & 0xFF 
-
-    if key == ord("q"):
-        break
-
+cam.release()
 cv2.destroyAllWindows()
